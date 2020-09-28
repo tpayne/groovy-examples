4e9ac6a0a31ae7e920b5f4370c4482123e02cb88
Tim Payne <tpayne@users.noreply.github.com>
2020-09-28 17:40:35 +0100

    Create json2map.groovy

---
+++ json2map.groovy
@@ -0,0 +1,62 @@
+//
+// Groovy script for getting json map key value
+//
+import groovy.json.*
+import groovy.xml.*
+
+class JsonMapper {
+
+    // Pass the string to Groovy JSON slurper to convert to map
+    static def convertJsonToMap(def json) {
+        def slurp = new JsonSlurper()
+        return slurp.parseText(json)
+    }
+
+    // Recurse map to find value
+    static def recurseFind(def map, def key) {
+        if (map.containsKey(key)) return map[key]
+        map.findResult { k, v -> v instanceof Map ? recurseFind(v, key) : null }
+    }
+
+    // Main routine
+    static void main(String[] args) { 
+        def keyTxt
+        def txt = null
+        def file = null
+
+        // Check args...
+        if (args.size()>0) {
+            for(int n=0; n<args.size(); n++) {
+                if (args[n].equals("--json-key")) {
+                    keyTxt = args[n+1]
+                } else if (args[n].equals("--json-text")) {
+                    txt = args[n+1]
+                } else if (args[n].equals("--json-file")) {
+                    file = args[n+1]
+                }
+            }
+        }
+        
+        if (file != null) {
+            txt = new File(file.toString()).text
+        }
+
+        // Convert JSON to map...
+        def map = convertJsonToMap(txt)
+
+        // Tokenize composite key...
+        def tokens = keyTxt.tokenize("/")
+        def mm = map
+
+        // Recurse map to find value...
+        tokens.each { key -> 
+            mm = recurseFind(mm, key)
+        }
+
+        if (mm==null) {
+            println "No match found"
+        } else {
+            println "Result='" + mm + "'"
+        }
+    }
+}
