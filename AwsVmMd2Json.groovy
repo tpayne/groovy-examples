a79d833eae595b4c3871f5942431eb3de98ea288
Tim Payne <tpayne@users.noreply.github.com>
2020-09-28 22:58:36 +0100

    Update AwsVmMd2Json.groovy

--- AwsVmMd2Json.groovy
+++ AwsVmMd2Json.groovy
@@ -15,7 +15,7 @@ class AWSMetaDump {
         def outTxt
 
         try {
-            outTxt = new URL(uri.toString()).getText()      
+            outTxt = new URL(uri.toString()).getText()
             outTxt = outTxt.replaceAll("\n"," ")
         } catch(Exception e) {
         }
@@ -52,19 +52,52 @@ class AWSMetaDump {
         return (pretty) ? builder.toPrettyString() : builder.toString()
     }
 
+    // Recurse map to find value
+    static def recurseFind(def map, def key) {
+        if (map != null) {
+            if (map.containsKey(key)) return map[key]
+            map.findResult { k, v -> v instanceof Map ? recurseFind(v, key) : null }
+        } else {
+            return null
+        }
+    }
+
     // Main routine
     static void main(String[] args) { 
         def uri = urlString
+        def queryStr = null
 
         if (args.size()>0) {
-            uri = args[0]
+        	for(int n=0; n<args.size(); n++) {
+        		if (args[n].equals("--url")) {
+					uri = args[n+1]
+				} else if (args[n].equals("--query-key")) {
+					queryStr = args[n+1]
+				}
+        	}
         }
         
         // Get the AWS metadata in collasped form...
         def output = scanAWSInstanceData(uri)
+
+        if (queryStr!=null) {
+            // Tokenize composite key...
+            def tokens = queryStr.tokenize("/")
+            def mm = output
+
+            // Recurse map to find value...
+            tokens.each { key -> 
+                mm = recurseFind(mm, key)
+            }   
+            output = mm         
+        }
         
         // Print the data out in JSON formats...
-        println "RAW JSON -> " + convertListToJSON(output)
-        println "PRETTY JSON -> " + convertListToJSON(output,true)
+        if (output != null) {
+            println "RAW JSON -> " + convertListToJSON(output)
+            println "PRETTY JSON -> " + convertListToJSON(output,true)
+        } else {
+            println "No match found"
+        }
     }
 }
